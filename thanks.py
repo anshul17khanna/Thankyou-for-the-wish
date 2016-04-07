544b53bfaa798ab3bc8d5f0bf99bf833f6395f08
Anshul Khanna <anshul17khanna@gmail.com>
2016-04-07 23:12:23 +0530

    api

--- thanks.py
+++ thanks.py
@@ -1,5 +1,9 @@
 import sys
 import socket
+import time
+import datetime
+
+access_token = ''  # Paste your token here
 
 REMOTE_SERVER = "www.google.com"
 def is_connected():
@@ -15,9 +19,18 @@ if is_connected() is False:
     sys.exit(1)
 
 try:
+    import fb
+except ImportError:
+    print "\nfb not installed, please install using :\npip install fb\n"
+    sys.exit(1)
+
+try:
     from facepy import GraphAPI
 except ImportError:
     print "\nfacepy not installed, please install using :\npip install facepy\n"
     sys.exit(1)
 
-
+facebook = fb.graph.api(access_token)
+graph = GraphAPI(access_token)
+response = graph.get('me/feed?since=' + str(int(time.time()) - (3600*int(datetime.datetime.now().hour) + 60*int(datetime.datetime.now().minute) + int(datetime.datetime.now().second)) + 1))
+# print response['data'][0]['from']
